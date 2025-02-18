fe928f9015569362df102c0db1c249c3c347f552
Michael Schrandt <michaelschrandt@Michaels-MacBook-Pro-2.local>
2025-02-17 17:52:43 -0800

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/dfs-golf-optimizer/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
